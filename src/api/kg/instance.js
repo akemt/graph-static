591833d408b4da4024f2186df562733acb2f7026
yaoyuan93 <cJkluio789>
2018-04-09 14:50:24 +0800

    1. 改名

--- src/api/kg/instance.js
+++ src/api/kg/instance.js
@@ -3,7 +3,7 @@ import config from '../config'
 
 const baseUrl = {
   mock: 'http://h2.beyondcloud.cn/mock/5ac19d725fc44d1824d39446/api',
-  dev: '',
+  dev: 'http://localhost:8080/',
   pre: '',
   prod: ''
 }[config.env || 'mock']
