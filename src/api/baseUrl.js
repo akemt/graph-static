5f5545df1a6b494ad40340c33d4729f70028cd23
matrix_xialf <592427777@qq.com>
2018-04-11 17:02:03 +0800

    get请求传参

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -2,7 +2,7 @@ import config from './config'
 
 export default {
   mock: 'http://h2.beyondcloud.cn/mock/5ac19d725fc44d1824d39446/api',
-  dev: 'http://localhost:8080/',
+  dev: 'http://localhost:8089',
   pre: '',
   prod: ''
 }[config.env || 'mock']
