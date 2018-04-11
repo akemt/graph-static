613e499e381e3d41a57a4725c8255014270994a5
liukeyan <390697811@qq.com>
2018-04-11 11:19:53 +0800

    新建实体完成
    Signed-off-by: liukeyan <390697811@qq.com>

--- src/views/edit/entity/index.vue
+++ src/views/edit/entity/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="app-container">
+    <el-button type="primary" plain @click="newEntity()" class="create-button">新建实体</el-button>
     <el-row>
       <el-col :span="8">
         <search ajax-path="entitys_get" root-path="xxx" @selected="editEntity"></search>
@@ -14,6 +15,8 @@
 <script>
   import search from '@/components/search/index'
 
+  const api = require('@/api/kg/index')
+
   export default {
     data() {
       return {
@@ -23,6 +26,12 @@
     methods: {
       editEntity(item) {
         this.$router.push({name: 'entity.item', params: {id: item.id}})
+      },
+      newEntity() {
+        api.entitys_post({json: '新建实体'}).then((data) => {
+          console.log('成功 ID：', data.data.id)
+          this.$router.push({name: 'entity.item', params: {id: data.data.id}})
+        })
       }
     },
     components: {search}
@@ -30,5 +39,8 @@
 </script>
 
 <style scoped>
+  .create-button{
+    margin-bottom: 5px;
+  }
 </style>
 
